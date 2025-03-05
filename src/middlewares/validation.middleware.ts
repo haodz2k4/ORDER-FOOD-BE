c6b36b6f5ed9eb1fd0015f786ce3abc1aa6af282
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2025-03-05 18:25:43 +0700

    Validate middleware

---
+++ src/middlewares/validation.middleware.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction } from "express";
+import Joi, { ObjectSchema } from "joi";
+
+
+
+export const validateRequest = (schema: {
+    body?: ObjectSchema,
+    query?: ObjectSchema,
+    param?: ObjectSchema
+}) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const options = { abortEarly: false, stripUnknown: true };
+        const errors: any = {};
+
+        if (schema.body) {
+            const { error, value } = schema.body.validate(req.body, options);
+            if (error) errors.body = error.details.map((err) => err.message);
+            else req.body = value;
+        }
+
+        if (schema.query) {
+            const { error, value } = schema.query.validate(req.query, options);
+            if (error) errors.query = error.details.map((err) => err.message);
+            else req.query = value;
+        }
+
+        if (schema.param) {
+            const { error, value } = schema.param.validate(req.params, options);
+            if (error) errors.params = error.details.map((err) => err.message);
+            else req.params = value;
+        }
+
+        if (Object.keys(errors).length) {
+            return res.status(400).json({ errors });
+        }
+
+        next();
+    };
+};
